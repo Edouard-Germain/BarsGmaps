901327f2884a47a9fb37cf7f326a9073d8315e41
Edouard-Germain <edangermain@gmail.com>
2022-01-25 16:58:34 +0100

    test

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { ChakraProvider } from "@chakra-ui/provider"
+import { Flex, Box, Center } from '@chakra-ui/react'
+
+import { MapContextProvider } from "./contexts/Map"
+import Map from "./components/Map"
+import List from './components/List'
+
+const App = () => {
+  return (
+    <ChakraProvider>
+      <MapContextProvider>
+        <Flex h="100vh" w="100%">
+          <Box h="100%" w="30%" p={5} overflowY="scroll">
+            <List />
+          </Box>
+          <Center background="teal" h="100%" w="70%">
+            <Map />
+          </Center>
+        </Flex>
+      </MapContextProvider>
+    </ChakraProvider>
+  )
+}
+
+export default App
---
+++ src/components/Map.js
@@ -0,0 +1,34 @@
+import { useContext } from 'react'
+
+import GoogleMapReact from 'google-map-react'
+import { MapContext } from '../contexts/Map'
+import Marker from './Marker'
+
+const Map = () => {
+  const { location, bars } = useContext(MapContext)
+
+  if (!location) {
+    return <p>Chargement</p>
+  }
+
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={location}
+        defaultZoom={12}
+      >
+        {bars.map(bar =>(
+          <Marker
+            key={bar.id}
+            lat={bar.latitude}
+            lng={bar.longitude}
+            bar={bar}
+          />
+        ))}
+      </GoogleMapReact>
+    </div>
+  )
+}
+
+export default Map
